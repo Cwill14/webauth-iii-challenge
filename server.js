6eea39621e90b7a31750c82dc9836eca417cf22e
Clark Williams <theclarkwilliams@gmail.com>
2019-08-21 15:12:57 -0600

    MVP

--- server.js
+++ server.js
@@ -2,12 +2,18 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 
+const authRouter = require('./auth/auth-router.js');
+const usersRouter = require('./users/users-router.js');
+
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 server.use(cors());
 server.use(logger);
+
+server.use('/auth', authRouter);
+server.use('/users', usersRouter);
 
 function logger(req, res, next) {
     const method = req.method;
